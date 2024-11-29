04d418b61dd0b2154b82e2d95e621c449aa7e055
oO0oO0oO0o0o00 <10877157+oO0oO0oO0o0o00@users.noreply.github.com>
2024-11-29 21:21:07 +0800

    Initial Commit

---
+++ Playground/PlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlaygroundApp.swift
+//  Playground
+//
+//  Created by MeowCat on 2024/11/29.
+//
+
+import SwiftUI
+
+@main
+struct PlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
